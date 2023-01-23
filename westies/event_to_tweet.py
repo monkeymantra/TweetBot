b50aaec3c65e3e3a9717470b8578bc02d416f785
Gregory Rice <gregrice@gmail.com>
2023-01-23 18:33:28 +0100

    Working ChatGPT integration

--- westies/event_to_tweet.py
+++ westies/event_to_tweet.py
@@ -1,39 +1,4 @@
 import datetime
-from dataclasses import dataclass
-
-import openai
-import os
-
-@dataclass
-class Prompt:
-    start_dt: datetime.datetime
-    end_dt: datetime.datetime
-    max_length: int = 160
-    content:    str = ""
-    location:   str = "Westies" # "Always going to be present in the text prompt
-
-    def to_prompt(self) -> str:
-        return f"""A tweet has a maximum length of 160 characters. Produce a fun tweet to advertise the following event,
-        targeting a college-aged demographic:
-        {self.content}
-        """
-
-class ChatGPTClient:
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    MODEL_ENGINE="text-davinci-003"
-
-    def __init__(self):
-        pass
-
-    def get_response(self, prompt: Prompt):
-        completion = openai.Completion.create(
-            engine=self.MODEL_ENGINE,
-            prompt=prompt,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
 
 
 class EventToTweet:
